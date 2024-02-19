e8f3db07ecfc9c5e9a09388dd62ccf37011a0e8c
davshi <davish.std@gmail.com>
2024-02-19 07:26:32 +0530

    gradle version error

--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,51 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+import 'features/app/splash_screen/splash_screen.dart';
+import 'features/user_auth/presentation/pages/home_page.dart';
+import 'features/user_auth/presentation/pages/login_page.dart';
+import 'features/user_auth/presentation/pages/sign_up_page.dart'; // Import Firebase Auth
+
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey: "AIzaSyBjBUxyoziQFZgmBgDKyNzj-FPNRzScnME",
+          authDomain: "flutter-cmr.firebaseapp.com",
+          projectId: "flutter-cmr",
+          storageBucket: "flutter-cmr.appspot.com",
+          messagingSenderId: "487428582219",
+          appId: "1:487428582219:web:0352da5e2ee6c7324916ce",
+          measurementId: "G-RL4HSJ3QMX"
+        // Your web Firebase config options
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  bool showBubble = false;
-  Offset bubbleOffset = Offset(20.0, 40.0);
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      title: 'HSI',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Boooooo'),
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Firebase',
+      routes: {
+        '/': (context) => SplashScreen(
+          // Here, you can decide whether to show the LoginPage or HomePage based on user authentication
+          child: LoginPage(),
         ),
-        body: Stack(
-          children: [
-            // Main content, buttons, etc. (replace with your actual content)
-            Center(
-              child: ElevatedButton.icon(
-                icon: Icon(Icons.play_arrow),
-                label: Text('Start'),
-                onPressed: () => setState(() => showBubble = true),
-              ),
-            ),
-            Visibility(
-              visible: showBubble,
-              child: Stack( // Use another Stack inside for better positioning
-                children: [
-                  Positioned( // Corrected placement for draggable bubble
-                    left: bubbleOffset.dx,
-                    top: bubbleOffset.dy,
-                    child: Draggable(
-                      child: _BubbleWidget(offset: bubbleOffset),
-                      feedback: _BubbleWidget(offset: bubbleOffset),
-                      childWhenDragging: Container(), // Hide while dragging
-                      onDragEnd: (dragDetails) => setState(() => bubbleOffset = dragDetails.offset),
-                    ),
-                  ),
-                  // Optional close button or other interactive elements
-                  // positioned correctly within the inner Stack
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class _BubbleWidget extends StatelessWidget {
-  final Offset offset;
-
-  const _BubbleWidget({Key? key, required this.offset}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Material( // Allow touch events to pass through
-      color: Colors.transparent,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10.0),
-          color: Colors.white,
-        ),
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Text('DAV'), // Customize with your desired content
-        ),
-      ),
+        '/login': (context) => LoginPage(),
+        '/signUp': (context) => SignUpPage(),
+        '/home': (context) => HomePage(),
+      },
     );
   }
 }
